a10642d7356f6e634e31cecc8da2a1e1a67c0dc9
hariharan481 <118888824+hariharan481@users.noreply.github.com>
2023-08-07 16:23:29 +0530

    Add files via upload

---
+++ src/components/IndexSearch/IndexSearch.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { useState } from "react";
+import"../styles/IndexSearch.css";
+import { DrugTables } from './DrugTables';
+import NeoplasmTable from './NeoplasamTable';
+import CircularWithValueLabel from './Spinner';
+import { useEffect } from 'react';
+import IndexTables1 from './IndexTable2';
+
+
+
+
+
+
+
+export const IndexSearch = () => {
+
+  const [value, setValue] = useState(0);
+  const [showTable, setShowTable] = useState(false);
+  const [showIndx, setShowIndex] = useState(false);
+  const [showDrug, setShowdrug] = useState(false);
+
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleIndexClick = () => {
+    setShowIndex(!showIndx);
+    setShowTable(false);
+    setShowdrug(false);
+  };
+  const handleTableClick = () => {
+    setShowTable(!showTable);
+    setShowIndex(false);
+    setShowdrug(false);
+  };
+  const handleDrugClick = () => {
+    setShowdrug(!showDrug);
+    setShowIndex(false);
+    setShowTable(false);
+  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    setShowIndex(false);
+    setShowTable(false);
+    setShowdrug(false);
+  };
+
+
+
+
+  const helloHandeler = () => {
+    setLoading(!loading);
+    setShow(!show);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 10000);
+    }
+  }, [loading]);
+
+  if (loading) return 
+
+  <CircularWithValueLabel />
+
+ 
+
+  return (
+    
+
+<div className="indexcontainer">
+      <button className="btn1"
+        onClick={handleIndexClick}
+        >
+        {showIndx ? "" : ""}
+        Index
+      </button>
+      <div className="IndexTablecontainer">
+
+      {showIndx && <IndexTables1/>}
+      </div>
+      <button  className="btn2"
+        onClick={handleTableClick}
+      >
+        {showTable ? "" : ""}
+        Neoplasam
+      </button>
+
+      <div className="neoplasmcontainer">
+
+      { showTable && <NeoplasmTable/>}
+</div>
+
+    
+      <button className="btn3"
+      
+        onClick={handleDrugClick}
+      >
+        {showDrug ? "" : ""}
+        Drugs
+      </button>
+      <div className="drugcontainer">
+
+
+      {showDrug && <DrugTables/>}
+      </div>
+    </div>
+  
+  )
+}
